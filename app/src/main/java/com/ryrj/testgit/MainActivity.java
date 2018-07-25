9be0601cb320a0333067c67f5c28937df3931a19
武瑞丰 <wuruifeng@pzfw.net>
2018-07-25 18:01:40 +0800

    初始化项目

--- app/src/main/java/com/ryrj/testgit/MainActivity.java
+++ app/src/main/java/com/ryrj/testgit/MainActivity.java
@@ -2,6 +2,7 @@ package com.ryrj.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void fun1(){
+        Toast.makeText(this, "fun1()", Toast.LENGTH_SHORT).show();
     }
 }
